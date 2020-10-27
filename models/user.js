a5e5308ad9e9ca4cc436f5759c7df79d301a76ed
Juan Rivas <one.rivas@gmail.com>
2020-10-27 16:06:58 -0700

    user token working

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  email: {type: String, required: true, unique: true},
+  password: {
+    type: String, required: true
+  }
+});
+
+userSchema.pre(
+  'save',
+  async function(next) {
+    const user = this;
+    const hash = await bcrypt.hash(this.password, 10);
+    console.log(user);
+
+    this.password = hash;
+    next();
+  }
+)
+userSchema.methods.isValidPassword = async function(password){
+  const user = this;
+  const compare = await bcrypt.compare(password, user.password);
+
+  return compare;
+}
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose =require('mongoose');
+const passport = require('passport')
 const app = express();
 require('dotenv').config();
 const bodyParser = require('body-parser');
@@ -25,14 +26,35 @@ mongoose.connection.on('connected', () =>
   console.log('mongo connected: ', MONGODB_URI)
 )
 mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
+mongoose.Promise = global.Promise;
+require('./auth/auth')
+
+const userModel = require('./models/user.js')
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 const petsController = require('./controllers/petsController.js');
 app.use('/pets', petsController)
 const todoController = require('./controllers/todoController.js');
 app.use('/todos', todoController)
+const authController = require('./controllers/authController.js')
+app.use('/auth', authController)
+
+
+const secureController = require('./controllers/secureRoutes.js')
+app.use('/user', passport.authenticate('jwt', { session: false }), secureController)
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({ error: err });
+})
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
